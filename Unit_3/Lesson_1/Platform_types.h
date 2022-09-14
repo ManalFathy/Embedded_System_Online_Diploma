07f77302f232c8bbb1dbd4d4cdf6b871683683eb
ManalFathy <110366848+ManalFathy@users.noreply.github.com>
2022-09-14 16:40:34 -0400

    Add lesson1 assignment

---
+++ Unit_3/Lesson_1/Platform_types.h
@@ -0,0 +1,24 @@
+#ifndef __types__
+#define __types__
+
+typedef signed char					sint8;
+typedef unsigned char				uint8;
+typedef signed short				sint16;
+typedef unsigned short				uint16;
+typedef signed int					sint32;
+typedef unsigned int				uint32;
+typedef signed long long			sint64;
+typedef unsigned long long			uint64;
+
+
+
+typedef volatile signed char					vint8_t;
+typedef volatile unsigned char					vuint8_t;
+typedef volatile signed short					vint16_t;
+typedef volatile unsigned short					vuint16_t;
+typedef volatile signed int						vint32_t;
+typedef volatile unsigned int					vuint32_t;
+typedef volatile signed long long				vint64_t;
+typedef volatile unsigned long long				vuint64_t;
+
+#endif
