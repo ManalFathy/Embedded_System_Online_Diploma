b65d3bec04e6a48a8688c94a758718ef54313cc1
ManalFathy <110366848+ManalFathy@users.noreply.github.com>
2022-12-27 17:42:28 -0500

    unit7lesson2section

---
+++ unit7/unit7lesson2section/Lcd_Keypad_Drivers/Lcd_Keypad_Drivers/main.c
@@ -0,0 +1,35 @@
+/*
+ * Lcd_Keypad_Drivers.c
+ *
+ * Created: 12/26/2022 3:39:57 PM
+ * Author :Manal
+ */ 
+
+#include <avr/io.h>
+#include "LCD_driver/lcd.h"
+#include "KEYPAD_driver/keypad.h"
+
+int main(void)
+{
+	LCD_INIT();
+	_delay_ms(10);
+	Keypad_init();
+	LCD_custom_characters();
+	char key_pressed;
+	
+        while (1) 
+    {
+		key_pressed = Keypad_getkey();
+		switch(key_pressed){
+			case 'A':
+			break;
+			case '?':
+			LCD_clear_screen();
+			break;
+			default:
+			LCD_WRITE_CHAR(key_pressed);
+			break;
+		}
+    }
+}
+
